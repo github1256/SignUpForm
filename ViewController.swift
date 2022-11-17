163188033428f0b8f759f8dd75ea09a0e7ec306f
github1256 <100110388+github1256@users.noreply.github.com>
2022-11-17 00:32:21 -0500

    Add files via upload
    new

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment 6 - Sign Up form
+//
+//  Created by Rubisha Shrestha on 11/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
